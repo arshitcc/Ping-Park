6d21c1f45cbeb57f44bd4bf0d066c11903b5ad31
Arshit Chaurasia <arshitcc@gmail.com>
2025-04-10 04:05:27 +0530

    feat(upload) : configure Multer and Cloudinary for file uploads

---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { ApiError } from "./ApiError";
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_FOLDER_NAME } from "./env";   
+
+cloudinary.config({
+    cloud_name : CLOUDINARY_CLOUD_NAME,
+    api_key : CLOUDINARY_API_KEY,
+    api_secret : CLOUDINARY_API_SECRET,
+})
+
+const uploadFile = async(filePath: string) => {
+    try {
+        const response = await cloudinary.uploader.upload(filePath,{
+            folder : CLOUDINARY_FOLDER_NAME,
+            resource_type : "auto",
+        });
+        fs.unlinkSync(filePath);
+        return response.url;
+    } catch (error:any) {
+        fs.unlinkSync(filePath);
+        throw new ApiError(false, 500, "Failed to upload file to cloudinary");
+    }
+}
+
+const deleteFile = async(fileId: string, resource_type : string) => {
+    try {
+        const publicId = fileId;
+        if(!publicId.trim() || !resource_type.trim()){
+            throw new ApiError(false, 400, "Invalid or Empty FileId or ResourceType");
+        }
+        if(publicId && resource_type){
+            const resposne =  await cloudinary.uploader.destroy(publicId,{ resource_type });
+            return resposne;
+        }
+        else return false;
+    } catch (error) {
+        throw new ApiError(false, 500, "Failed to delete file from cloudinary");
+    }
+}
+
+export { cloudinary, uploadFile, deleteFile };
